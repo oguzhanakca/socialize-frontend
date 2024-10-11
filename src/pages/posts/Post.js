f60518f0abc83acea12f421aa6c8ff4c0e59ae4c
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-11 18:51:09 +0200

    edit and delete dropdown added

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,9 +2,10 @@ import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, OverlayTrigger, Tooltip } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Post = (props) => {
   const {
@@ -25,6 +26,20 @@ const Post = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const navigate = useNavigate();
+
+  const handleEdit = () => {
+    navigate(`/posts/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`posts/${id}`);
+      navigate(-1);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const handleLike = async () => {
     try {
@@ -75,7 +90,9 @@ const Post = (props) => {
         </Link>
         <div className="d-flex align-items-center">
           <span>{updated_at}</span>
-          {is_owner && postPage && "..."}
+          {is_owner && postPage && (
+            <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete} />
+          )}
         </div>
       </Card.Header>
       <Link to={`/posts/${id}`}>
