ad1c7e8370434718474ab5b7fe2858017e119f95
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-11 20:33:21 +0200

    delete comment added

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -2,24 +2,62 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import axios from "axios";
 
 const Comment = (props) => {
-  const { profile_id, profile_image, owner, updated_at, content } = props;
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/comments/${id}`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
       <hr />
-      <div className="d-flex">
-        <Link to={`/profiles/${profile_id}`}>
-          <Avatar src={profile_image} />
-        </Link>
-        <div className="d-flex flex-column ml-2">
-          <div>
-            <span className={styles.Owner}>{owner}</span>
-            <span className={styles.Date}>{updated_at}</span>
+      <div className="d-flex justify-content-between">
+        <div className="d-flex">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} />
+          </Link>
+          <div className="d-flex flex-column ml-2 justify-content-between">
+            <div>
+              <span className={styles.Owner}>{owner}</span>
+              <span className={styles.Date}>{updated_at}</span>
+            </div>
+            <p>{content}</p>
           </div>
-
-          <p>{content}</p>
+        </div>
+        <div>
+          {is_owner && (
+            <MoreDropdown handleDelete={handleDelete} handleEdit={() => {}} />
+          )}
         </div>
       </div>
     </div>
