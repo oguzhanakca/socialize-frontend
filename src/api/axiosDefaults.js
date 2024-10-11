411c111693dc5871ff5c9fd52849d1e4c92e7ed8
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-11 04:15:46 +0200

    token refresh added

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,6 @@ axios.defaults.baseURL =
   "https://socialize-backend-app-9beed826b5f2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
