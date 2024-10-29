d22743e25951b81dc6d0e36445d3e7a5701d8265
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-29 22:37:17 +0100

    404 page updated

--- src/pages/error/NotFound.js
+++ src/pages/error/NotFound.js
@@ -6,6 +6,7 @@ function NotFound() {
     <div className={`${styles.Page} d-flex flex-column mt-5 text-center`}>
       <h2 className={styles.Head}>404</h2>
       <h3 className={styles.Description}>The Page Not Found</h3>
+      <h4 className="fs-5">Please navigate using the navbar.</h4>
     </div>
   );
 }
