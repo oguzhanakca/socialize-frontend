ea709413085718bb62c66eb0988cb1d346c38a78
oguzhanakca <oguzhan.akca@hotmail.com>
2024-10-31 16:55:59 +0100

    api route default changed

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  "https://socialize-backend-app-9beed826b5f2.herokuapp.com/api/";
+  "https://socialize-backend-app-9beed826b5f2.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
